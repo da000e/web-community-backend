844c559e61ad1d9f31b0239f953046ed1e4d200e
JUNNU06 <dadada000e@gmail.com>
2026-02-14 01:20:01 +0900

    init: web-community-backend

---
+++ src/main/java/com/junu/web_community_backend/WebCommunityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.junu.web_community_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCommunityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCommunityBackendApplication.class, args);
+	}
+
+}
